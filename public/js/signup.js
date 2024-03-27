b9940b34bff8da7253fb03261132d8cef5604654
Domj1204 <148828606+Domj1204@users.noreply.github.com>
2024-03-27 14:29:32 -0500

    updated the signup.js

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,27 +1,36 @@
+/**
+ * @function signupFormHandler
+ * @param {*} event 
+ * 
+ * Handles the event the user is signing up to create an account
+ */
 async function signupFormHandler(event) {
     event.preventDefault();
-
-    const username = document.querySelector('#username-signup').value.trim();
+  
+	// Collect username, email, and password from signup form fields
+    const username = document.querySelector('#name-signup').value.trim();
+    const email = document.querySelector('#email-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
+  
+	// Verify the values are not empty
+    if (username && email && password) {
+		// Call the userRoutes.js POST api route to create a user and log them in
+		const response = await fetch('/api/users', {
+			method: 'POST',
+			body: JSON.stringify({ username, email, password }),
+			headers: { 'Content-Type': 'application/json' },
+   		});
+  
+		if (response.ok) {
+			document.location.replace('/dashboard');
+		} else {
+			alert('Failed to sign up.');
+		}
+    } else {
+		document.location.reload();
+		alert("Please enter username, password, and email.");
+	}
+};
 
-    if (username && password) {
-        const response = await fetch('/api/users', {
-            method: 'post',
-            body: JSON.stringify({
-                username,
-                password
-            }),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        });
-
-        if (response.ok) {
-            document.location.replace('/dashboard');
-        } else {
-            alert(response.statusText);
-        }
-    }
-}
-
+/* Add event listener */
 document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
