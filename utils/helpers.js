da7b3cac09ccea899d97bc61060a06b480d0a0db
Domj1204 <148828606+Domj1204@users.noreply.github.com>
2024-03-27 14:34:21 -0500

    updated the helpers.js

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,10 +1,9 @@
-function formatDate(date) {
-    return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(
-        date
-      ).getFullYear()}`;
-}
-
-
 module.exports = {
-    formatDate
-}
+	/**
+	 * @function format_date
+	 * @param {*} date 
+	 * @returns {String} formatted date 
+	 * Formats the date parameter to M/D/YYYY
+	 */
+    format_date(date) {
+      	return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(date).getFullYear()}`;
