ac2a9f3e97e96692b14ed3ae3be430c29a018c0d
Domj1204 <148828606+Domj1204@users.noreply.github.com>
2024-03-27 14:25:21 -0500

    updated the addPost.js

--- public/js/addPost.js
+++ public/js/addPost.js
@@ -1,25 +1,39 @@
-async function newFormHandler(event) {
-    event.preventDefault();
-
-    const title = document.querySelector('input[name="post-title"]').value;
-    const post_content = document.querySelector('textarea[name="post-content"]').value.trim();
-
-    const response = await fetch(`/api/posts`, {
-        method: 'POST',
-        body: JSON.stringify({
-            title,
-            post_content
-        }),
-        headers: {
-            'Content-Type': 'application/json'
+/**
+ * @function newPostFormHandler
+ * @param {*} event 
+ * 
+ * Handles the event the user is adding a new blog post in add-post.handlebars
+ */
+async function newPostFormHandler(event) {
+	event.preventDefault();
+  
+    // Get title and content from input fields
+    const title = document.getElementById('post-title').value;
+    const content = document.getElementById('post-content').value;
+  
+    // Check title and content are not empty 
+    if (title && content) {
+        // Call the postRoutes.js POST api route to create a new post in the database
+        const response = await fetch(`/api/posts`, {
+            method: 'POST',
+            body: JSON.stringify({
+                title,
+                content
+            }),
+            headers: {'Content-Type': 'application/json'}
+        });
+      
+        // Redirect the user back to the dashboard page when the post has been created
+        if (response.ok) {
+              document.location.replace('/dashboard');
+        } else {
+              alert(response.statusText);
         }
-    });
-
-    if (response.ok) {
-        document.location.replace('/dashboard');
     } else {
-        alert(response.statusText);
+        // reload the page and notify the user title and content fields may not be empty 
+        document.location.reload();
+        alert('New post title and content cannot be blank.');
     }
 }
-
-document.querySelector('.new-post-form').addEventListener('submit', newFormHandler);
+  
+document.querySelector('.new-post-form').addEventListener('submit', newPostFormHandler);
