4ca4468998535575ebf9151efce2f5e3e876e6c3
Domj1204 <148828606+Domj1204@users.noreply.github.com>
2024-03-27 14:03:39 -0500

    updated the dashboardRoutes.js

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -1,105 +1,154 @@
+/**
+ * @file dashboardRoutes.js
+ * Implements the dashboard page API routes to render handlebars files
+ * 
+ * @see  ../views/dashboard.handlebars
+ */
+
 const router = require('express').Router();
 const sequelize = require('../config/connection');
-const {
-    Post,
-    User,
-    Comment
-} = require('../models');
+const { Post, User, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 
-
-router.get('/', withAuth, (req, res) => {
-    Post.findAll({
+/** 
+ * @route GET '/dashboard'
+ * Checks the session user, finds the post data for the user, and returns data to render in dashboard.handlebars
+ */
+router.get('/', withAuth, async (req, res) => {
+    try { 
+        const postData = await Post.findAll({
             where: {
+                // Filter the posts using user_id defined in the session
                 user_id: req.session.user_id
             },
             attributes: [
                 'id',
                 'title',
                 'content',
-                'created_at'
+                'created_date'
             ],
-            include: [{
-                    model: Comment,
-                    attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-                    include: {
-                        model: User,
-                        attributes: ['username']
-                    }
-                },
+            order: [['created_date', 'DESC']],
+            include: [
                 {
                     model: User,
-                    attributes: ['username']
+                    attributes: ['id', 'username']
+                },
+                {
+                    model: Comment,
+                    attributes: ['id', 'content', 'created_date'],
+                    include: {
+                        model: User,
+                        attributes: ['id', 'username']
+                    }
                 }
             ]
-        })
-        .then(dbPostData => {
-            const posts = dbPostData.map(post => post.get({
-                plain: true
-            }));
-            res.render('dashboard', {
-                posts,
-                loggedIn: true
-            });
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
         });
+
+        // Serialize and render the data in .handlebars
+        const posts = postData.map((post) => post.get({ plain: true }));
+
+        res.render('dashboard', {
+            posts,
+            loggedIn: req.session.loggedIn
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
-router.get('/edit/:id', withAuth, (req, res) => {
-    Post.findOne({
+/**
+ * @route GET '/dashboard/addPost'
+ * Checks the session user, finds the post data for the user, and returns data to render in add-post.handlebars
+ */
+router.get('/addPost', withAuth, async (req, res) => {
+    try {
+        const postData = await Post.findAll({
             where: {
-                id: req.params.id
+                // Filter the posts using user_id defined in the session
+                user_id: req.session.user_id
             },
             attributes: [
                 'id',
                 'title',
                 'content',
-                'created_at'
+                'created_date'
             ],
-            include: [{
-                    model: Comment,
-                    attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-                    include: {
-                        model: User,
-                        attributes: ['username']
-                    }
-                },
+            include: [
                 {
                     model: User,
-                    attributes: ['username']
+                    attributes: ['id', 'username']
+                },
+                {
+                    model: Comment,
+                    attributes: ['id', 'content', 'created_date'],
+                    include: {
+                        model: User,
+                        attributes: ['id', 'username']
+                    }
                 }
             ]
-        })
-        .then(dbPostData => {
-            if (!dbPostData) {
-                res.status(404).json({
-                    message: 'No post found with this id'
-                });
-                return;
-            }
-
-            const post = dbPostData.get({
-                plain: true
-            });
-
-            res.render('edit-post', {
-                post,
-                loggedIn: true
-            });
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
         });
+
+        // Serialize and render the data in add-post.handlebars
+        const posts = postData.map(post => post.get({ plain: true }));
+        res.render('add-post', {
+            posts,
+            loggedIn: true
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+
 })
 
-router.get('/new', (req, res) => {
-    res.render('add-post', {
-        loggedIn: true
-    })
+/**
+ * @route GET '/update/:id'
+ * Checks the session user, finds the post data by id for the user, and returns data to render in update-post.handlebars
+ */
+router.get('/update/:id', withAuth, async (req, res) => {
+    try {
+        const postData = await Post.findOne({
+            where: {
+                id: req.params.id,
+            },
+            attributes: [
+                'id',
+                'title',
+                'content',
+                'created_date'
+            ],
+            include: [
+                { 
+                    model: User,
+                    attributes: ['id', 'username'],
+                },
+                {
+                    model: Comment,
+                    attributes: ['id', 'content', 'created_date'],
+                    include: {
+                        model: User,
+                        attributes: ['id', 'username']
+                    }
+                }
+            ]
+        });
+
+        if (!postData) {
+            res.status(404).json({
+                message: 'No post data was found for the requested id.'
+            });
+            return;
+        }
+
+        // Serialize and render the data in update-post.handlebars
+        const post = postData.get({ plain: true });
+        res.render('update-post', {
+            post,
+            loggedIn: true,
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
 })
 
 module.exports = router;
