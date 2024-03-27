7c4b0a9a0738eaf16ba6eae990ece91f7eda41ac
Domj1204 <148828606+Domj1204@users.noreply.github.com>
2024-03-27 14:27:38 -0500

    updaed the login.js

--- public/js/login.js
+++ public/js/login.js
@@ -1,27 +1,36 @@
+/**
+ * @function loginFormHandler
+ * @param {*} event 
+ * 
+ * Handles the event the user is logging in with their credentials
+ */
 async function loginFormHandler(event) {
     event.preventDefault();
 
-    const username = document.querySelector('#username-login').value.trim();
+    // Collect email and password values from the login form fields
+    const email = document.querySelector('#email-login').value.trim();
     const password = document.querySelector('#password-login').value.trim();
+  
+	// Verify that email and password is not empty
+    if (email && password) {
+      	// Call the userRoute.js POST /login API route to validate credentials and start the session
+      	const response = await fetch('/api/users/login', {
+			method: 'POST',
+			body: JSON.stringify({ email, password }),
+			headers: { 'Content-Type': 'application/json' },
+    	});
 
-    if (username && password) {
-        const response = await fetch('/api/users/login', {
-            method: 'post',
-            body: JSON.stringify({
-                username,
-                password
-            }),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        });
-
-        if (response.ok) {
-            document.location.replace('/dashboard');
-        } else {
-            alert(response.statusText);
-        }
-    }
-}
-
+		// If successful, redirect the browser to the dashboard page
+		if (response.ok) {
+			document.location.replace('/dashboard');
+		} else {
+			alert('Failed to login.');
+		}
+	} else {
+		document.location.reload();
+		alert("Please enter username and password.");
+	}
+};
+  
+/* Add event listener */
 document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
