efcb1f6c1b5aea18afd03c73b0b7e80046ef490b
Domj1204 <148828606+Domj1204@users.noreply.github.com>
2024-03-27 13:51:45 -0500

    updated the controllers/api/index.js file

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 
-const userRoutes = require('./user-routes.js');
-const postRoutes = require('./post-routes.js');
-const commentRoutes = require('./comments-routes.js');
+const userRoutes = require('./userRoutes');
+const postRoutes = require('./postRoutes');
+const commentRoutes = require('./commentsRoutes');
 
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
