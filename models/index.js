a918cecb006c2c887f6b0b6add30a4f52921cc45
Domj1204 <148828606+Domj1204@users.noreply.github.com>
2024-03-27 14:16:07 -0500

    updated both the User.js & the index.js

--- models/index.js
+++ models/index.js
@@ -1,32 +1,39 @@
-const User = require('./user');
+/**
+ * @file index.js
+ * Executes association methods on the Sequelize models to create relationships between them
+ */
+
+/* Import the Sequelize models */
+const User = require('./User');
 const Post = require('./Post');
 const Comment = require('./Comment');
 
-
+/* `User` model hasMany `Posts` and `Comments` */
 User.hasMany(Post, {
-    foreignKey: 'user_id'
-})
+    foreignKey: 'user_id',
+});
 
 User.hasMany(Comment, {
-    foreignKey: 'user_id'
-})
+    foreignKey: 'user_id',
+});
 
+/* `Post` model belongs to `User` and has many `Comments` */
 Post.belongsTo(User, {
-    foreignKey: 'user_id'
-})
+    foreignKey: 'user_id',
+});
 
 Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+});
+
+// `Comment` model belongs to `Post` and to `User`
+Comment.belongsTo(Post, {
     foreignKey: 'post_id'
 })
 
 Comment.belongsTo(User, {
     foreignKey: 'user_id'
 })
-
-Comment.belongsTo(Post, {
-    foreignKey: 'post_id'
-})
-
 
 module.exports = {
     User,
